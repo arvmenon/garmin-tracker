a85c612358cdfb96a06de5fec664d160298d6ad5
arvmenon <arvmenon@users.noreply.github.com>
2025-12-19 11:30:28 -0500

    Add netdata role to db bootstrap

--- docker/db-init/00-bootstrap-db.sh
+++ docker/db-init/00-bootstrap-db.sh
@@ -7,11 +7,15 @@ APP_DB_USER=${APP_DB_USER:-garmin_app}
 APP_DB_PASSWORD=${APP_DB_PASSWORD:-garmin_app}
 READONLY_DB_USER=${READONLY_DB_USER:-garmin_readonly}
 READONLY_DB_PASSWORD=${READONLY_DB_PASSWORD:-garmin_readonly}
+NETDATA_DB_USER=${NETDATA_DB_USER-netdata}
+NETDATA_DB_PASSWORD=${NETDATA_DB_PASSWORD-netdata}
 APP_DB_NAME_ESCAPED=$(printf "%s" "$APP_DB_NAME" | sed "s/'/''/g")
 APP_DB_USER_ESCAPED=$(printf "%s" "$APP_DB_USER" | sed "s/'/''/g")
 APP_DB_PASSWORD_ESCAPED=$(printf "%s" "$APP_DB_PASSWORD" | sed "s/'/''/g")
 READONLY_DB_USER_ESCAPED=$(printf "%s" "$READONLY_DB_USER" | sed "s/'/''/g")
 READONLY_DB_PASSWORD_ESCAPED=$(printf "%s" "$READONLY_DB_PASSWORD" | sed "s/'/''/g")
+NETDATA_DB_USER_ESCAPED=$(printf "%s" "$NETDATA_DB_USER" | sed "s/'/''/g")
+NETDATA_DB_PASSWORD_ESCAPED=$(printf "%s" "$NETDATA_DB_PASSWORD" | sed "s/'/''/g")
 
 "${PSQL_BIN}" -v ON_ERROR_STOP=1 \
      --username "$POSTGRES_USER" \
@@ -24,6 +28,8 @@ DECLARE
     app_password text := '${APP_DB_PASSWORD_ESCAPED}';
     readonly_user text := '${READONLY_DB_USER_ESCAPED}';
     readonly_password text := '${READONLY_DB_PASSWORD_ESCAPED}';
+    netdata_user text := '${NETDATA_DB_USER_ESCAPED}';
+    netdata_password text := '${NETDATA_DB_PASSWORD_ESCAPED}';
 BEGIN
     IF NOT EXISTS (
         SELECT FROM pg_catalog.pg_roles WHERE rolname = app_user
@@ -35,6 +41,13 @@ BEGIN
         SELECT FROM pg_catalog.pg_roles WHERE rolname = readonly_user
     ) THEN
         EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', readonly_user, readonly_password);
+    END IF;
+
+    IF netdata_user <> '' AND NOT EXISTS (
+        SELECT FROM pg_catalog.pg_roles WHERE rolname = netdata_user
+    ) THEN
+        EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', netdata_user, netdata_password);
+        EXECUTE format('GRANT pg_monitor TO %I', netdata_user);
     END IF;
 
     IF NOT EXISTS (
@@ -57,6 +70,7 @@ DECLARE
     app_db text := '${APP_DB_NAME_ESCAPED}';
     app_user text := '${APP_DB_USER_ESCAPED}';
     readonly_user text := '${READONLY_DB_USER_ESCAPED}';
+    netdata_user text := '${NETDATA_DB_USER_ESCAPED}';
 BEGIN
     EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', app_db, app_user);
     EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', app_db, readonly_user);
@@ -70,6 +84,10 @@ BEGIN
     EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO %I', readonly_user);
     EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO %I', app_user);
     EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO %I', readonly_user);
+
+    IF netdata_user <> '' THEN
+        EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', app_db, netdata_user);
+    END IF;
 END
 $$;
 SQL
