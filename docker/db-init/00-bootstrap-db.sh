5b2468f0226a48d9e399d9a20fa7b5151a0890dc
arvmenon <arvmenon@users.noreply.github.com>
2025-12-20 21:23:41 -0500

    Remove netdata database bootstrap

--- docker/db-init/00-bootstrap-db.sh
+++ docker/db-init/00-bootstrap-db.sh
@@ -19,21 +19,14 @@ APP_DB_USER=${APP_DB_USER:-garmin_app}
 APP_DB_PASSWORD=${APP_DB_PASSWORD:-garmin_app}
 READONLY_DB_USER=${READONLY_DB_USER:-garmin_readonly}
 READONLY_DB_PASSWORD=${READONLY_DB_PASSWORD:-garmin_readonly}
-NETDATA_DB_USER=${NETDATA_DB_USER-netdata}
-NETDATA_DB_PASSWORD=${NETDATA_DB_PASSWORD-netdata}
 APP_DB_NAME_ESCAPED=$(printf "%s" "$APP_DB_NAME" | sed "s/'/''/g")
 APP_DB_USER_ESCAPED=$(printf "%s" "$APP_DB_USER" | sed "s/'/''/g")
 APP_DB_PASSWORD_ESCAPED=$(printf "%s" "$APP_DB_PASSWORD" | sed "s/'/''/g")
 READONLY_DB_USER_ESCAPED=$(printf "%s" "$READONLY_DB_USER" | sed "s/'/''/g")
 READONLY_DB_PASSWORD_ESCAPED=$(printf "%s" "$READONLY_DB_PASSWORD" | sed "s/'/''/g")
-NETDATA_DB_USER_ESCAPED=$(printf "%s" "$NETDATA_DB_USER" | sed "s/'/''/g")
-NETDATA_DB_PASSWORD_ESCAPED=$(printf "%s" "$NETDATA_DB_PASSWORD" | sed "s/'/''/g")
 
 log_debug "[db-init] Bootstrapping database roles"
-log_debug "[db-init] app_db=${APP_DB_NAME} app_user=${APP_DB_USER} readonly_user=${READONLY_DB_USER} netdata_user=${NETDATA_DB_USER}"
-if [ -z "$NETDATA_DB_USER" ]; then
-    log_debug "[db-init] netdata role creation disabled (NETDATA_DB_USER is empty)"
-fi
+log_debug "[db-init] app_db=${APP_DB_NAME} app_user=${APP_DB_USER} readonly_user=${READONLY_DB_USER}"
 
 "${PSQL_BIN}" -v ON_ERROR_STOP=1 \
      --username "$POSTGRES_USER" \
@@ -46,8 +39,6 @@ DECLARE
     app_password text := '${APP_DB_PASSWORD_ESCAPED}';
     readonly_user text := '${READONLY_DB_USER_ESCAPED}';
     readonly_password text := '${READONLY_DB_PASSWORD_ESCAPED}';
-    netdata_user text := '${NETDATA_DB_USER_ESCAPED}';
-    netdata_password text := '${NETDATA_DB_PASSWORD_ESCAPED}';
 BEGIN
     IF NOT EXISTS (
         SELECT FROM pg_catalog.pg_roles WHERE rolname = app_user
@@ -59,13 +50,6 @@ BEGIN
         SELECT FROM pg_catalog.pg_roles WHERE rolname = readonly_user
     ) THEN
         EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', readonly_user, readonly_password);
-    END IF;
-
-    IF netdata_user <> '' AND NOT EXISTS (
-        SELECT FROM pg_catalog.pg_roles WHERE rolname = netdata_user
-    ) THEN
-        EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', netdata_user, netdata_password);
-        EXECUTE format('GRANT pg_monitor TO %I', netdata_user);
     END IF;
 
     IF NOT EXISTS (
@@ -88,7 +72,6 @@ DECLARE
     app_db text := '${APP_DB_NAME_ESCAPED}';
     app_user text := '${APP_DB_USER_ESCAPED}';
     readonly_user text := '${READONLY_DB_USER_ESCAPED}';
-    netdata_user text := '${NETDATA_DB_USER_ESCAPED}';
 BEGIN
     EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', app_db, app_user);
     EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', app_db, readonly_user);
@@ -102,10 +85,6 @@ BEGIN
     EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO %I', readonly_user);
     EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO %I', app_user);
     EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO %I', readonly_user);
-
-    IF netdata_user <> '' THEN
-        EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', app_db, netdata_user);
-    END IF;
 END
 \$\$;
 SQL
